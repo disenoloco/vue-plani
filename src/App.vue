3f27e3c3b6f7e5b732e791edf286de83cf440e97
disenoloco <ydh2945@gmail.com>
2020-01-15 23:51:02 +0900

    뭐

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <!-- 
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter> 
+    -->
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader.vue'
+  import TodoInput from './components/TodoInput.vue'
+  import TodoList from './components/TodoList.vue'
+  import TodoFooter from './components/TodoFooter.vue'
+
+  export default {
+    components: {
+      'TodoHeader': TodoHeader,
+      'TodoInput': TodoInput,
+      'TodoList': TodoList,
+      'TodoFooter': TodoFooter
+   }
+  }
+</script>
+<style>
+  #app { font-family: 'Open Sans', sans-serif; }
+  body { text-align: center; background-color: #f6f6f6; }
+  input { border-style: groove; width: 90%; }
+  button { border-style: groove; }
+  .shadow { box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.08); }
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do." v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <transition name="fade">
+        <i class="addBtn xi-plus" aria-hidden="true"></i>
+      </transition>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: { 
+    addTodo() {
+      if(this.newTodoItem !== ""){
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);  
+      }
+      this.clearInput();
+    },
+    clearInput(){
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+<style>
+  input:focus { outline: none; }
+  .inputBox { background: white; max-width: 1000px; height: 50px; line-height: 50px; margin: 0 auto; border-radius: 5px; }
+  .inputBox input { border-style: none; font-size: .9rem; }
+  .addContainer { float: right; background: linear-gradient(to right, #6478FB, #8763FB); display: block; width: 3rem; height: 100%; border-radius: 0 5px 5px 0; cursor: pointer; }
+  .addBtn { color: #fff; vertical-align: middle; font-size: 1.2rem; }
+</style>
